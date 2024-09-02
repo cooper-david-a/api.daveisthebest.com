02fdc4012f873f542fd72a11398a4b762277a09f
Dave <dave@davestechnicalservices.com>
2024-09-02 15:35:27 -0500

    add send email on davestech_contacts viewset

--- davestech_contacts/views.py
+++ davestech_contacts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ContactSerializer
+from django.core.mail import send_mail
+import logging
+
+logger=logging.getLogger(__name__)
+
 
 from .models import Contact
 
@@ -8,3 +13,16 @@ class ContactViewSet(ModelViewSet):
     http_method_names = ["post", "head", "options"]
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            email_from = 'dave@davestechnicalservices.com'
+            email_to = [request.data['email'], 'dave@davestechnicalservices.com']
+            subject = 'Contact from davestechnicalservices.com'
+            message = request.data['message']
+            send_mail(subject, message, email_from, email_to, fail_silently=False)
+        except Exception as e:
+            logger.debug(type(e))
+            logger.debug(e)            
+
+        return super().create(request, *args, **kwargs)
