f1845197cc98eb648b1c918968cf3dfaed960d10
Dave <cooper.david.a@proton.me>
2026-04-24 06:20:40 -0500

    add filter by result and next/prev page to admin

--- coinflipper/filters.py
+++ coinflipper/filters.py
@@ -6,7 +6,8 @@ class CoinFlipFilter(django_filters.FilterSet):
     flipped_after = django_filters.DateTimeFilter(field_name='flipped_at', lookup_expr='gte')
     flipped_before = django_filters.DateTimeFilter(field_name='flipped_at', lookup_expr='lte')
     flipped_date = django_filters.DateFilter(field_name='flipped_at', lookup_expr='date')
+    result = django_filters.ChoiceFilter(choices=[('H', 'Heads'), ('T', 'Tails'), ('U', 'Unknown')])
 
     class Meta:
         model = CoinFlip
-        fields = ['flipped_after', 'flipped_before', 'flipped_date']
+        fields = ['flipped_after', 'flipped_before', 'flipped_date', 'result']
