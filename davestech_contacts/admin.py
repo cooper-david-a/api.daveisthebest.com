2c92ba21b58192d359ef0f245a681e17c0765d25
Dave Cooper <cda_public@pm.me>
2026-05-23 15:34:41 -0500

    make davestech_contacts message readable

--- davestech_contacts/admin.py
+++ davestech_contacts/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+from django import forms
 from .models import Contact
 
-# Register your models here.
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ["id", "created_at", "name", "email"]
     list_per_page = 25
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == "message":
+            kwargs["widget"] = forms.Textarea(attrs={"rows": 10, "cols": 80})
+        return super().formfield_for_dbfield(db_field, **kwargs)
 
